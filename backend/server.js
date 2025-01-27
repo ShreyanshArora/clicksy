df15f85d8fba4716544397249988fb3af1df3312
Shreyansh Arora <shreyansharora69@gmail.com>
2025-01-28 01:15:04 +0530

    project setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app=express();
+const port=process.env.port || 5000;
+
+app.use("/api/auth",authRoutes);
+
+app.listen(port,()=>{
+    console.log("server is running on https://localhost:"+port);
+});
