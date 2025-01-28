2df0d10d41eef7090b553db4c56f1be3bd0ccb73
Shreyansh Arora <shreyansharora69@gmail.com>
2025-01-28 19:15:44 +0530

    models and hashing done

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,10 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app=express();
-const port=process.env.port || 5000;
+const port=process.env.PORT || 5000;
 
+app.use(express.json()); //parse the body of the request
+app.get("/",(req,res)=>{res.send("Hello World")});
 app.use("/api/auth",authRoutes);
 
 app.listen(port,()=>{
