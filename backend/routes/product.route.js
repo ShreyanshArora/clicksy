aeadb250b2ee92ce72eb8c0b7c6a89a422aca62e
Shreyansh Arora <shreyansharora69@gmail.com>
2025-02-02 00:31:54 +0530

    featured products routes and controller

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
-import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllProducts , getFeaturedProducts} from "../controllers/product.controller.js";
+import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router=express.Router();
 
-router.get("/",protectRoute ,adminRoute , getAllProducts)
+router.get("/", protectRoute , adminRoute , getAllProducts);
 
-export default router
+router.get("/featured", getFeaturedProducts);
+
+
+export default router;
