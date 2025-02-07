1e88c2200fd630b16a0809e92803a35be51c494a
Shreyansh Arora <shreyansharora69@gmail.com>
2025-02-07 17:41:37 +0530

    payment coupon done

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 
 dotenv.config();
 
@@ -14,10 +17,16 @@ const port=process.env.PORT || 5000;
 app.use(express.json()); //parse the body of the request
 app.use(cookieParser());
 
-app.get("/",(req,res)=>{res.send("Hello World")});
+
 app.use("/api/auth",authRoutes);
 
 app.use("/api/products",productRoutes);
+
+app.use("/api/cart",cartRoutes);
+
+app.use("/api/coupon",couponRoutes);
+
+app.use("/api/payments",paymentRoutes);
 
 
 app.listen(port,()=>{
