51f7385070c47788a6c216b93f989b82092777c8
Shreyansh Arora <shreyansharora69@gmail.com>
2025-02-18 00:42:52 +0530

    analytics,giftCoupon and more

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -16,7 +16,7 @@ export const useUserStore = create((set, get) => ({
 		}
 
 		try {
-			const res = await axios.post("/auth/signup", { name, email, password });
+			const res = await axios.post("/auth/SignUp", { name, email, password });
 			set({ user: res.data, loading: false });
 		} catch (error) {
 			set({ loading: false });
